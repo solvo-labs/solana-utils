0329804e44767e391b7f5b582ad8f4e942ad4f86
afarukcali <afarukcali@gmail.com>
2023-07-14 16:42:17 +0300

    withdrawStake

--- stake.js
+++ stake.js
@@ -73,4 +73,22 @@ const deactivateStake = async () => {
   console.log(deactivateTxId);
 };
 
-deactivateStake();
+const withdrawStake = async () => {
+  const stakeBalance = await connection.getBalance(stakeAccount);
+
+  const withdrawTx = StakeProgram.withdraw({
+    stakePubkey: stakeAccount,
+    authorizedPubkey: payer.publicKey,
+    toPubkey: payer.publicKey,
+    lamports: stakeBalance, // Withdraw the full balance at the time of the transaction
+  });
+
+  const withdrawTxId = await sendAndConfirmTransaction(connection, withdrawTx, [payer]);
+  console.log(`Stake account withdrawn. Tx Id: ${withdrawTxId}`);
+
+  // Confirm that our stake account balance is now 0
+  stakeBalance = await connection.getBalance(stakeAccount.publicKey);
+  console.log(`Stake account balance: ${stakeBalance / LAMPORTS_PER_SOL} SOL`);
+};
+
+withdrawStake();
